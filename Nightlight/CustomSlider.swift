cdad2f827318f4cc398635f53c7c673360901e1c
steven <sarmiento.steven@gmail.com>
2024-02-09 23:35:42 -0500

    building custom slider and some organization

---
+++ Nightlight/CustomSlider.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSlider.swift
+//  Nightlight
+//
+//  Created by Steven Sarmiento on 2/9/24.
+//
+
+import Foundation
---
+++ Nightlight/NightlightMvp.swift
@@ -0,0 +1,8 @@
+//
+//  NightlightMvp.swift
+//  Nightlight
+//
+//  Created by Steven Sarmiento on 2/9/24.
+//
+
+import Foundation
