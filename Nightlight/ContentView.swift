8f3499b90a17317a101f3863c539fd4afab01c4f
Steven Sarmiento <sarmiento.steven@gmail.com>
2024-02-09 20:02:39 -0500

    Initial Commit

---
+++ Nightlight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nightlight
+//
+//  Created by Steven Sarmiento on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nightlight/NightlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightlightApp.swift
+//  Nightlight
+//
+//  Created by Steven Sarmiento on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NightlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
